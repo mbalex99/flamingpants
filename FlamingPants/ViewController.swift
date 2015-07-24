2456bb3aacd5c1bc29e9e6b77cf7c1da50410147
Maximilian Alexander <max@zinkpulse.com>
2015-07-24 08:24:17 -0700

    adding icons and included the default imports

--- FlamingPants/ViewController.swift
+++ FlamingPants/ViewController.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import RxSwift
 import SlackTextViewController
 
-class ViewController: UIViewController {
+class ViewController: SLKTextViewController {
     
 
     override func viewDidLoad() {
